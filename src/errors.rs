77a3507e1999d11869779c449c725dbb148bd9a7
Adam Lesperance <lespea@gmail.com>
2019-06-23 14:49:10 -0500

    Almost got dumping the win events so we can test

---
+++ src/errors.rs
@@ -0,0 +1,77 @@
+use std::fmt::{Display, Error, Formatter};
+use winapi::shared::winerror::*;
+use winapi::um::errhandlingapi::GetLastError;
+use windows_error::WindowsError;
+
+#[derive(Debug)]
+pub struct WinEvtError {
+    pub errno: u32,
+    pub msg: String,
+}
+
+impl Display for WinEvtError {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        f.write_str(self.msg.as_str())
+    }
+}
+
+impl WinEvtError {
+    pub fn from_last_error() -> Self {
+        Self::from_dword(unsafe { GetLastError() })
+    }
+
+    pub fn from_dword(errno: u32) -> Self {
+        let msg: String = match errno {
+            ERROR_EVT_CANNOT_OPEN_CHANNEL_OF_QUERY => "cannot open channel of query".to_string(),
+            ERROR_EVT_CHANNEL_CANNOT_ACTIVATE => "channel cannot activate".to_string(),
+            ERROR_EVT_CHANNEL_NOT_FOUND => "channel not found".to_string(),
+            ERROR_EVT_CONFIGURATION_ERROR => "configuration error".to_string(),
+            ERROR_EVT_EVENT_DEFINITION_NOT_FOUND => "event definition not found".to_string(),
+            ERROR_EVT_EVENT_TEMPLATE_NOT_FOUND => "event template not found".to_string(),
+            ERROR_EVT_FILTER_ALREADYSCOPED => "filter alreadyscoped".to_string(),
+            ERROR_EVT_FILTER_INVARG => "filter invarg".to_string(),
+            ERROR_EVT_FILTER_INVTEST => "filter invtest".to_string(),
+            ERROR_EVT_FILTER_INVTYPE => "filter invtype".to_string(),
+            ERROR_EVT_FILTER_NOTELTSET => "filter noteltset".to_string(),
+            ERROR_EVT_FILTER_OUT_OF_RANGE => "filter out of range".to_string(),
+            ERROR_EVT_FILTER_PARSEERR => "filter parseerr".to_string(),
+            ERROR_EVT_FILTER_TOO_COMPLEX => "filter too complex".to_string(),
+            ERROR_EVT_FILTER_UNEXPECTEDTOKEN => "filter unexpectedtoken".to_string(),
+            ERROR_EVT_FILTER_UNSUPPORTEDOP => "filter unsupportedop".to_string(),
+            ERROR_EVT_INVALID_CHANNEL_PATH => "invalid channel path".to_string(),
+            ERROR_EVT_INVALID_CHANNEL_PROPERTY_VALUE => {
+                "invalid channel property value".to_string()
+            }
+            ERROR_EVT_INVALID_EVENT_DATA => "invalid event data".to_string(),
+            ERROR_EVT_INVALID_OPERATION_OVER_ENABLED_DIRECT_CHANNEL => {
+                "invalid operation over enabled direct channel".to_string()
+            }
+            ERROR_EVT_INVALID_PUBLISHER_NAME => "invalid publisher name".to_string(),
+            ERROR_EVT_INVALID_PUBLISHER_PROPERTY_VALUE => {
+                "invalid publisher property value".to_string()
+            }
+            ERROR_EVT_INVALID_QUERY => "invalid query".to_string(),
+            ERROR_EVT_MALFORMED_XML_TEXT => "malformed xml text".to_string(),
+            ERROR_EVT_MAX_INSERTS_REACHED => "max inserts reached".to_string(),
+            ERROR_EVT_MESSAGE_ID_NOT_FOUND => "message id not found".to_string(),
+            ERROR_EVT_MESSAGE_LOCALE_NOT_FOUND => "message locale not found".to_string(),
+            ERROR_EVT_MESSAGE_NOT_FOUND => "message not found".to_string(),
+            ERROR_EVT_NON_VALIDATING_MSXML => "non validating msxml".to_string(),
+            ERROR_EVT_PUBLISHER_DISABLED => "publisher disabled".to_string(),
+            ERROR_EVT_PUBLISHER_METADATA_NOT_FOUND => "publisher metadata not found".to_string(),
+            ERROR_EVT_QUERY_RESULT_INVALID_POSITION => "query result invalid position".to_string(),
+            ERROR_EVT_QUERY_RESULT_STALE => "query result stale".to_string(),
+            ERROR_EVT_SUBSCRIPTION_TO_DIRECT_CHANNEL => {
+                "subscription to direct channel".to_string()
+            }
+            ERROR_EVT_UNRESOLVED_PARAMETER_INSERT => "unresolved parameter insert".to_string(),
+            ERROR_EVT_UNRESOLVED_VALUE_INSERT => "unresolved value insert".to_string(),
+            ERROR_EVT_VERSION_TOO_NEW => "version too new".to_string(),
+            ERROR_EVT_VERSION_TOO_OLD => "version too old".to_string(),
+
+            _ => WindowsError::new(errno).to_string(),
+        };
+
+        WinEvtError { errno, msg }
+    }
+}
---
+++ src/event_iter.rs
@@ -0,0 +1,94 @@
+use std::collections::VecDeque;
+use std::ptr::null_mut;
+
+use widestring;
+use winapi;
+use winapi::shared::minwindef::DWORD;
+use winapi::shared::ntdef::NULL;
+use winapi::um::winbase::INFINITE;
+use winapi::um::winevt::{self, EvtClose, EvtNext, EvtQuery, EVT_HANDLE};
+
+use crate::errors::WinEvtError;
+
+const EVENTS_BUFFER: usize = 10;
+
+pub struct WinEventsIter {
+    query_handle: EVT_HANDLE,
+    done: bool,
+    events: VecDeque<EVT_HANDLE>,
+}
+
+impl Iterator for WinEventsIter {
+    type Item = Result<EVT_HANDLE, WinEvtError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.done {
+            return None;
+        } else if !self.events.is_empty() {
+            return self.events.pop_front().map(Ok);
+        }
+
+        let mut returned: DWORD = 0;
+        let mut next: [EVT_HANDLE; EVENTS_BUFFER] = [null_mut(); EVENTS_BUFFER];
+
+        let ret = unsafe {
+            EvtNext(
+                self.query_handle,
+                EVENTS_BUFFER as u32,
+                &mut next as *mut _ as _,
+                INFINITE,
+                0,
+                &mut returned,
+            )
+        };
+
+        if ret != 0 {
+            self.done = true;
+            return Some(Err(WinEvtError::from_dword(ret as u32)));
+        }
+
+        self.events.extend(next.iter().take(returned as usize));
+        self.next()
+    }
+}
+
+impl Drop for WinEventsIter {
+    fn drop(&mut self) {
+        if unsafe { EvtClose(self.query_handle) } != 0 {
+            panic!(format!(
+                "Couldn't close a windows event handle: {}",
+                WinEvtError::from_last_error(),
+            ))
+        }
+    }
+}
+
+impl WinEventsIter {
+    pub fn get_logs_for(name: &str, query: Option<&str>) -> Result<WinEventsIter, WinEvtError> {
+        let path = widestring::U16String::from_str(name);
+
+        let query = match query {
+            None => std::ptr::null(),
+            Some(q) => widestring::U16String::from_str(q).as_ptr(),
+        };
+
+        let handle = unsafe {
+            EvtQuery(
+                std::ptr::null_mut(),
+                path.as_ptr(),
+                query,
+                winevt::EvtQueryChannelPath | winevt::EvtQueryForwardDirection,
+            )
+        };
+
+        if handle == NULL {
+            Err(WinEvtError::from_last_error())
+        } else {
+            Ok(WinEventsIter {
+                query_handle: handle,
+                events: VecDeque::with_capacity(EVENTS_BUFFER),
+                done: false,
+            })
+        }
+    }
+}
