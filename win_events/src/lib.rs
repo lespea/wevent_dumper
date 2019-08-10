0e3beb09e199f9cf43a72d27075ea525543318dc
Adam Lesperance <lespea@gmail.com>
2019-08-09 23:48:18 -0500

    Finish the renderer

--- win_events/src/lib.rs
+++ win_events/src/lib.rs
@@ -1,11 +1,11 @@
-pub mod channel_iter;
 #[macro_use]
 extern crate slog;
 
+//pub mod channel_iter;
 pub mod errors;
-pub mod event_iter;
-pub mod pub_metadata;
-pub mod pub_metadata_fetcher;
+//pub mod event_iter;
+//pub mod pub_metadata;
+//pub mod pub_metadata_fetcher;
 pub mod pub_metadata_fields;
 pub mod renderer;
 pub mod utils;
--- win_events/src/renderer.rs
+++ win_events/src/renderer.rs
@@ -2,11 +2,11 @@ use std::ptr;
 
 use winapi::um::winevt::{self, EvtRender};
 
-use crate::errors::WinError;
 use crate::errors::WinEvtError;
 use crate::utils;
 use crate::win_event::WinEvent;
 
+/// Turns a windows event into an xml string, reusing the buffer for each render
 pub struct Renderer {
     buf: Vec<u16>,
 }
@@ -18,20 +18,23 @@ impl Default for Renderer {
 }
 
 impl Renderer {
+    /// New renderer with 32k of buffer
     pub fn new() -> Self {
         Self::with_capacity(1024 * 32)
     }
 
+    /// New renderer with the specified buffer
     pub fn with_capacity(cap: usize) -> Self {
         Renderer {
             buf: Vec::with_capacity(cap),
         }
     }
 
+    /// Render the windows event as xml
     pub fn render(&mut self, we: WinEvent) -> Result<String, WinEvtError> {
         let mut buf_used = 0;
 
-        if let Err(e) = utils::check_okay_check(unsafe {
+        match utils::check_bool(unsafe {
             EvtRender(
                 ptr::null_mut(),
                 we.handle,
@@ -42,22 +45,22 @@ impl Renderer {
                 ptr::null_mut(),
             )
         }) {
-            return match e {
-                WinError::InsufficientBuffer => {
-                    self.buf.clear();
-                    self.buf.reserve((buf_used / 2) as usize + 1);
-                    self.render(we)
-                }
+            Err(WinEvtError::InsufficientBuffer) => {
+                self.buf.clear();
+                self.buf.reserve((buf_used / 2) as usize + 1);
+                return self.render(we);
+            }
 
-                _ => Err(e.into_err()),
-            };
-        }
+            Err(e) => return Err(e),
+
+            _ => (),
+        };
 
         // We need # of u16 but it returns "bytes" so u8 which means we need half of this
         let mut buf_used = (buf_used / 2) as usize;
 
         // See if there is a null byte at end. Should be but double check just in case
-        if unsafe { self.buf.as_ptr().add(buf_used as usize - 1).read() } == 0 {
+        if self.buf[buf_used] == 0 {
             buf_used -= 1;
         }
 
